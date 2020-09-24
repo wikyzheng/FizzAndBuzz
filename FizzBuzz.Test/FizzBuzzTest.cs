e154f1ba6ee949da91fd0dbbedcd2cd0dd9b0455
wiky <wikyzheng@qq.com>
2020-09-24 18:03:40 +0800

    unittest update

--- FizzBuzz.Test/FizzBuzzTest.cs
+++ FizzBuzz.Test/FizzBuzzTest.cs
@@ -11,11 +11,13 @@ namespace FizzBuzz.Test
         public void ShowFizzBuzzNumbersTest()
         {
             NumberProcess numberProcess = new NumberProcess();
+
+
             numberProcess.ShowFizzBuzzNumbers(0).ShouldBe("the input number must be larger than zero.");
 
             numberProcess.ShowFizzBuzzNumbers(10).ShouldBe("1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz");
 
-            numberProcess.ShowFizzBuzzNumbers(50).ShouldBe("1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz Fizz 14 Fizz 16 17 Fizz 19 Buzz Fizz 22 Fizz Fizz Buzz 26 Fizz 28 29 Fizz Fizz Fizz Fizz Fizz Fizz Fizz Fizz Fizz Fizz Buzz 41 Fizz Fizz 44 Fizz 46 47 Fizz 49 Buzz");
+            numberProcess.ShowFizzBuzzNumbers(50).ShouldBe("1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz Fizz 14 FizzBuzz 16 17 Fizz 19 Buzz Fizz 22 Fizz Fizz Buzz 26 Fizz 28 29 FizzBuzz Fizz Fizz Fizz Fizz Buzz Fizz Fizz Fizz Fizz Buzz 41 Fizz Fizz 44 FizzBuzz 46 47 Fizz 49 Buzz");
         }
     }
 }
